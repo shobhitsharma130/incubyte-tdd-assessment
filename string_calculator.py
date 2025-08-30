0134e1378e40dc6f9781b090fae83dca8020c72f
shobhitsharma130 <shobhitsharma130@gmail.com>
2025-08-30 13:41:06 +0530

    feat: support multple delimeters like //[delim1][delim2]\n

--- string_calculator.py
+++ string_calculator.py
@@ -5,16 +5,20 @@ def add(numbers: str) -> int:
         return 0
     delimiter = ","
     if numbers.startswith("//"):
-        delimiter_line,numbers = numbers.split('\n',1)
+        delimiter_line, numbers = numbers.split("\n", 1)
         if delimiter_line.startswith("//["):
             delimiters = re.findall(r"\[(.*?)\]", delimiter_line)
-            delimiter = "|".join(delimiters)
+            delimiter_pattern = "|".join(map(re.escape, delimiters))
         else:
-            delimiter = delimiter_line[2:]
+            # single custom delimiter, e.g. //;
+            delimiter_pattern = re.escape(delimiter_line[2:])
+    else:
+        delimiter_pattern = delimiter
+    tokens = re.split(delimiter_pattern + "|\n", numbers)
+    parts = [int(num) for num in tokens if num]
 
-    normalized = numbers.replace("\n",delimiter)
-    negatives = [n for n in normalized.split(delimiter) if int(n) < 0]
+    negatives = [n for n in parts if int(n) < 0]
     if negatives:
         raise ValueError(f"Negatives not allowed: {negatives}")
-    parts = [int(num) for num in normalized.split(delimiter) if int(num) <= 1000 ]
+    parts = [n for n in parts if n <= 1000]
     return sum(parts)
