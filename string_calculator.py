9506192a80d461fff116e0d45148947595dbcac4
shobhitsharma130 <shobhitsharma130@gmail.com>
2025-08-30 12:45:26 +0530

    feat: support custom single-char delimeters and newlines

--- string_calculator.py
+++ string_calculator.py
@@ -1,6 +1,11 @@
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
-    normalized = numbers.replace("\n",",")
-    parts = [int(num) for num in normalized.split(",")]
+    delimeter = ","
+    if numbers.startswith("//"):
+        delimeter_line,numbers = numbers.split('\n',1)
+        delimeter = delimeter_line[2:]
+
+    normalized = numbers.replace("\n",delimeter)
+    parts = [int(num) for num in normalized.split(delimeter)]
     return sum(parts)
